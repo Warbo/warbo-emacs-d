8622c608f55024b293ce113a6f2d918ebec684c5
Chris Warburton <chriswarbo@gmail.com>
2015-11-25 20:58:44 +0000

    Make sure Markdown is set up

--- tests/writing-tests.el
+++ tests/writing-tests.el
@@ -7,3 +7,12 @@
     (should     visual-line-mode)
     (should     flyspell-mode)
     (should-not whitespace-mode)))
+
+(ert-deftest warbo-markdown-comfortable ()
+  "Make sure the desired modes, etc. are activated for Markdown files."
+  (with-temp-buffer
+    (rename-buffer "test.md")
+    (set-auto-mode)
+    (should     visual-line-mode)
+    (should     flyspell-mode)
+    (should-not whitespace-mode)))
