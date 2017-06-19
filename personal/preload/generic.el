187178415446a735542a7b6ac4392185c4741811
Chris Warburton <chriswarbo@gmail.com>
2017-06-19 12:22:39 +0100

    Force use-package to install

--- personal/preload/generic.el
+++ personal/preload/generic.el
@@ -1,6 +1,10 @@
 (defun defer (f)
   (run-with-idle-timer 2 nil f))
 
+(unless (package-installed-p 'use-package)
+  (package-refresh-contents)
+  (package-install 'use-package))
+
 ;; Turn off pesky scrollbars
 (scroll-bar-mode -1)
 (menu-bar-mode -1)
