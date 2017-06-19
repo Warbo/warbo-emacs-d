08567f78d1e0e6b2e35473282fb5c3f7a18de8d3
Chris Warburton <chriswarbo@gmail.com>
2017-06-19 12:34:05 +0100

    Bootstrap MELPA

--- personal/preload/generic.el
+++ personal/preload/generic.el
@@ -1,6 +1,13 @@
 (defun defer (f)
   (run-with-idle-timer 2 nil f))
 
+(require 'package)
+(setq package-enable-at-startup nil)
+(add-to-list 'package-archives
+             '("melpa" . "https://melpa.org/packages/"))
+
+(package-initialize)
+
 (unless (package-installed-p 'use-package)
   (package-refresh-contents)
   (package-install 'use-package))
