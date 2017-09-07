029821790be71c4c8769a703f91ccc3dbc0acb06
Chris Warburton <chriswarbo@gmail.com>
2017-09-07 17:59:11 +0100

    Make quelpa and quelpa-use-package available
    use-package is a really useful macro for managing dependencies of our Emacs
    config. Unfortunately it can only use Emacs package repositories "out of the
    box", and there's a bunch of useful code out there on the Web which isn't
    "properly" packaged.
    
    quelpa lets us use these as if they were full packages.

--- personal/preload/generic.el
+++ personal/preload/generic.el
@@ -1,17 +1,27 @@
 (defun defer (f)
+  "Defer calling the function F until Emacs has finished initialising."
   (run-with-idle-timer 2 nil f))
 
+;; Set up packaging, first with Emacs's built-in "package.el" functionality
 (require 'package)
 (setq package-enable-at-startup nil)
 (add-to-list 'package-archives
              '("melpa" . "https://melpa.org/packages/"))
-
 (package-initialize)
 
+;; Next we use "package.el" to get "use-package", which simplifies packaging
 (unless (package-installed-p 'use-package)
   (package-refresh-contents)
   (package-install 'use-package))
 
-;; Turn off pesky scrollbars
+;; Get quelpa-use-package, which allows 'packages' to come from arbitrary places
+(unless (package-installed-p 'quelpa-use-package)
+  (package-refresh-contents)
+  (package-install 'quelpa-use-package))
+
+(use-package quelpa-use-package
+  :init (setq quelpa-update-melpa-p nil))
+
+;; Turn off UI clutter
 (scroll-bar-mode -1)
 (menu-bar-mode -1)
