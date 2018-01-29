f6996cda405b3253fd900ca1439638ff2c677204
Chris Warburton <chriswarbo@gmail.com>
2018-01-29 17:01:18 +0000

    Remove old cruft

--- personal/flycheck-custom.el
+++ personal/flycheck-custom.el
@@ -4,8 +4,7 @@
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
  '(dired-dwim-target t)
- '(jabber-libnotify-method (quote shell))
- '(jira-url "https://jira.wandisco.com/rpc/xmlrpc"))
+ '(jabber-libnotify-method (quote shell)))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
