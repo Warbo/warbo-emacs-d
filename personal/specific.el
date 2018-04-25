ac341fcfb4bb01d65d65e0a97318a9e0b8a42f85
Chris Warburton <chriswarbo@gmail.com>
2018-04-25 17:15:05 +0100

    Format Haskell files on save

--- personal/specific.el
+++ personal/specific.el
@@ -27,7 +27,18 @@
 (add-hook 'haskell-mode-hook 'flycheck-mode)
 ;(add-hook 'haskell-mode-hook 'interactive-haskell-mode)
 
-(custom-set-variables '(haskell-stylish-on-save t))
+(custom-set-variables '(haskell-stylish-on-save nil))
+
+(defun haskell-indent ()
+  "Send Haskell buffers through indentation program."
+  ;; Check that we've been "manually saved" (not auto-saved) and that we're a
+  ;; Haskell file (see https://emacs.stackexchange.com/a/14476/5391 )
+  (when (and (memq this-command '(save-buffer save-some-buffers))
+             (eq major-mode 'haskell-mode))
+    (shell-command-to-string (format "brittany %s" buffer-file-name))
+    (revert-buffer nil t t)))
+
+(add-hook 'after-save-hook 'haskell-indent)
 
 (eval-after-load "haskell-mode"
   '(define-key haskell-mode-map (kbd "C-c C-c") 'haskell-compile))
