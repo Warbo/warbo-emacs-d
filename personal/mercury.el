34c1e06964bbb5af6a56f251c7d342bd6c536b58
Chris Warburton <chriswarbo@gmail.com>
2018-09-19 17:33:30 +0100

    Add metal-mercury-mode

---
+++ personal/mercury.el
@@ -0,0 +1,6 @@
+;; Support for the Mercury logic programming language
+
+(use-package metal-mercury-mode
+  :quelpa (metal-mercury-mode :fetcher github
+                              :repo    "ahungry/metal-mercury-mode")
+  :mode "\\.m\\'")
