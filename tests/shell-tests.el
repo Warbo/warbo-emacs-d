78ff71a87faa5231d2a6adc93452717c473986c0
Chris Warburton <chriswarbo@gmail.com>
2019-01-09 21:46:38 +0000

    Disable laptop-specific tests when we're not running as me

--- tests/shell-tests.el
+++ tests/shell-tests.el
@@ -1,12 +1,14 @@
-(ert-deftest warbo-shells-exist ()
-  "Shell directories exist"
-  (dolist (pair startup-shells)
-    (should (file-accessible-directory-p (cadr pair)))))
+;; Only bother checking shell directories if we're on my laptop ;)
+(when (equal (getenv "HOME") "/home/chris")
+  (ert-deftest warbo-shells-exist ()
+    "Shell directories exist"
+    (dolist (pair startup-shells)
+      (should (file-accessible-directory-p (cadr pair)))))
 
-(ert-deftest warbo-shells-open ()
-  "Startup shells are open"
-  (dolist (pair startup-shells)
-    (should (member (car pair) (mapcar 'buffer-name (buffer-list))))))
+  (ert-deftest warbo-shells-open ()
+    "Startup shells are open"
+    (dolist (pair startup-shells)
+      (should (member (car pair) (mapcar 'buffer-name (buffer-list)))))))
 
 (ert-deftest warbo-shell-unique ()
   "Shell names don't overlap"
