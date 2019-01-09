d38e10772f054630969d6f0806620c381a6da362
Chris Warburton <chriswarbo@gmail.com>
2019-01-09 21:29:36 +0000

    Add a mercury compiling command

--- personal/mercury.el
+++ personal/mercury.el
@@ -1,6 +1,27 @@
 ;; Support for the Mercury logic programming language
 
+(defun compile-mercury ()
+  (interactive)
+  (let* ((file-name  (file-name-nondirectory (buffer-file-name)))
+         (is-mercury (equal "m" (file-name-extension file-name)))
+         (contracted (file-name-sans-extension file-name)))
+    (compile (cond
+              ((file-exists-p "default.nix") "nix-build")
+              ((file-exists-p "Makefile"     "make -k"  ))
+              (is-mercury     (concat "nix-shell -p mercury --run 'mmc --make "
+                                      contracted
+                                      "'"))
+              (t (error "Not a Mercury '.m' file"))))))
+
 (use-package metal-mercury-mode
   :quelpa (metal-mercury-mode :fetcher github
                               :repo    "ahungry/metal-mercury-mode")
-  :mode "\\.m\\'")
+  :mode "\\.m\\'"
+
+  :bind ("<f9>" . compile-mercury)
+
+  :config (setq metal-mercury-mode-compile-function
+                (lambda (module-name)
+                  (cl-concatenate 'string
+                                  "nix-shell -p mercury "
+                                  "--run 'mmc --make " module-name "'"))))
