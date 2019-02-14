70c344cb0a0ef48b46cc6dadf7155464caa2021a
Chris Warburton <chriswarbo@gmail.com>
2019-02-14 17:01:35 +0000

    Invoke artemis-mode when emacsclient invoked by artemis
    This should be more robust and faster than our previous solution, which invoked
    emacsclient twice with a hard-coded 1 second sleep.

--- personal/artemis.el
+++ personal/artemis.el
@@ -19,11 +19,43 @@
   "Major mode for editing Artemis issues (which are maildir under the hood)."
   (set-syntax-table artemis-mode-syntax-table))
 
-(provide 'artemis-mode)
-
 (defun switch-to-artemis (filename)
   "Look for a buffer with the given FILENAME and switch it to artemis-mode."
   (let ((buf (find-buffer-visiting filename)))
     (when buf
       (with-current-buffer buf
         (artemis-mode)))))
+
+(defun buffer-contains-substring (s)
+  "Helper function: does string S appear in the current buffer?
+From https://stackoverflow.com/a/3034272/884682"
+  (save-excursion
+    (save-match-data
+      (goto-char (point-min))
+      (search-forward s nil t))))
+
+(defun is-artemis-buffer-p (&optional buffer)
+  "Is the given BUFFER (default: 'current-buffer') an artemis issue?"
+  (with-current-buffer (or buffer (current-buffer))
+    (let ((f (buffer-file-name)))
+      (and
+       (string-prefix-p "/tmp/tmp" f)
+       (string-suffix-p ".txt"     f)
+       (buffer-contains-substring "Subject: brief description")))))
+
+(defun server-visit-artemis-issue-hook ()
+  "Invokes 'artemis-mode' if the current buffer is an artemis issue."
+  (when (is-artemis-buffer-p)
+    (artemis-mode)))
+
+;; This hook fires when emacsclient visits a file, checking if the visited file
+;; is an artemis issue, and if so invoking artemis-mode. We use this hook
+;; because:
+;;  - We cannot rely on filenames to spot issues, since they're all '.txt'
+;;  - We cannot call artemis-mode when invoking emacsclient, since opening files
+;;    and evaluating lisp are mutually exclusive options
+;;  - We cannot have emacsclient evaluate lisp to open the file and invoke
+;;    artemis-mode, since it will exit immediately and hence not work as EDITOR
+(add-hook 'server-visit-hook 'server-visit-artemis-issue-hook)
+
+(provide 'artemis-mode)
