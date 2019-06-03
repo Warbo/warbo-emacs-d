b417a94faa8cef48022c12f5216ed9477c56aed7
Chris Warburton <chriswarbo@gmail.com>
2019-06-04 00:27:14 +0100

    Add mu4e action for viewing message in browser

--- personal/mail.el
+++ personal/mail.el
@@ -152,3 +152,21 @@
 ;; compose with the current context is no context matches;
 ;; default is to ask
 ;; '(setq mu4e-compose-context-policy nil)
+
+;; From https://gist.github.com/syl20bnr/4460279
+;; adapted from https://groups.google.com/d/topic/mu-discuss/ZXB72TR5GL0/discussion
+(defun mu4e-msgv-action-view-in-browser (msg)
+  "View the body of the message in a web browser."
+  (interactive)
+  (let ((html (mu4e-msg-field (mu4e-message-at-point t) :body-html))
+        (tmpfile (format "%s/%d.html" temporary-file-directory (random))))
+    (unless html (error "No html part for this message"))
+    (with-temp-file tmpfile
+      (insert
+       "<html>"
+       "<head><meta http-equiv=\"content-type\""
+       "content=\"text/html;charset=UTF-8\">"
+       html))
+    (browse-url (concat "file://" tmpfile))))
+(add-to-list 'mu4e-view-actions
+             '("View in browser" . mu4e-msgv-action-view-in-browser) t)
