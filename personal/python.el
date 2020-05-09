cd4ca6a95fbf8165e4a7846df34a130792293e96
Chris Warburton <chris@zipabout.com>
2020-05-09 15:24:02 +0100

    Add some python things
    elpy is disabled for now, since it insists on using virtualenv, when we'd rather
    use nix, so it slows everything down with its polling and timeouts.

---
+++ personal/python.el
@@ -0,0 +1,29 @@
+(use-package python
+  :ensure t
+  :config
+  (custom-set-variables '(python-shell-interpreter "python3")
+                        '(python-shell-buffer-name "Python3")))
+
+;; (use-package pyvenv
+;;   :ensure t
+;;   :config
+;;   (setq pyvenv-exec-shell "zsh"))
+
+(use-package blacken
+  :ensure t)
+
+(use-package elpy
+  :disabled
+  :ensure t
+  :defer t
+  :init
+  (advice-add 'python-mode :before 'elpy-enable)
+  :config
+  (setq elpy-get-info-from-shell t
+        python-shell-interpreter "/run/current-system/sw/bin/python3")
+  (custom-set-variables
+   '(elpy-rpc-python-command "/run/current-system/sw/bin/python3")
+   '(elpy-test-discover-runner-command
+     '("/run/current-system/sw/bin/python3" "-m" "unittest")))
+  (add-to-list 'python-shell-completion-native-disabled-interpreters
+               "/run/current-system/sw/bin/python3"))
