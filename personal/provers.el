21365ba8d522d655bd83822a23bffea76f31a7cb
Chris Warburton <chris@zipabout.com>
2020-10-08 10:54:32 +0100

    Coq/PG

--- personal/provers.el
+++ personal/provers.el
@@ -7,3 +7,18 @@
 (defun init-pg ()
   "Load ProofGeneral from Nix location."
   (ignore-errors (load-file "~/.nix-profile/share/emacs/site-lisp/ProofGeneral/generic/proof-site.el")))
+
+(use-package proof-general
+  :ensure t
+  :mode ("\\.v\\'" . coq-mode)
+  :custom-face
+  ;(proof-locked-face ((t (:background "#add8e6"))))
+  )
+
+(use-package company-coq
+  :ensure t
+  :hook
+  (coq-mode . company-coq-mode)
+  :init
+  ;(setq company-coq-disabled-features '(hello prettify-symbols))
+  )
