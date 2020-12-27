f47ac6a74ff5ebda1e7c576eb20ac96a23f43cdd
Chris Warburton <chris@zipabout.com>
2020-12-27 14:41:00 +0000

    Remove old prelude functionality
    I tried it, and it's not what I want. No need to keep it around

--- personal/mac.el
+++ personal/mac.el
@@ -3,19 +3,6 @@
 
  (setq ns-function-modifier 'hyper)
 
- (defun prelude-swap-meta-and-super ()
-   "Swap the mapping of Meta and Super. Very useful for people using their Mac
-    with a Windows external keyboard from time to time."
-   (interactive)
-   (if (eq mac-command-modifier 'super)
-       (progn
-         (setq mac-command-modifier 'meta)
-         (setq mac-option-modifier 'super)
-         (message "Command is now bound to META and Option is bound to SUPER."))
-     (progn
-       (setq mac-command-modifier 'super)
-       (setq mac-option-modifier 'meta)
-       (message "Command is now bound to SUPER and Option is bound to META."))))
 
  ;; Fix Option-3 to give # on UK Mac keyboard. Emacs sees Option as Alt (AKA
  ;; Meta AKA M) which Emacs will intercept and complain that it isn't a command
@@ -32,6 +19,5 @@
  (if (fboundp 'set-fontset-font)
      (set-fontset-font t 'unicode "Apple Color Emoji" nil 'prepend))
 
-  ;(define-key prelude-mode-map (kbd "C-c w") 'prelude-swap-meta-and-super)
   ;(define-key prelude-mode-map (kbd "s-/") 'hippie-expand)
  )
