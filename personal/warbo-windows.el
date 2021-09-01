40e7edae5eda71f0535a8bbf1b88ef5afd2fba83
Chris Warburton <chris@zipabout.com>
2021-09-01 14:31:29 +0100

    Balance window sizes

--- personal/warbo-windows.el
+++ personal/warbo-windows.el
@@ -5,47 +5,46 @@
 ;;;
 ;;; Code:
 
-;; TODO: Set sizes
 (defun windows-tall ()
   (interactive)
-  ;; +-+
-  ;; | |
-  ;; +-+
+  ;; ┌─┐
+  ;; │ │
+  ;; └─┘
   (delete-other-windows)
 
-  ;; +-+
-  ;; | |
-  ;; +-+
-  ;; | |
-  ;; +-+
+  ;; ┌─┐
+  ;; │ │
+  ;; ├─┤
+  ;; │ │
+  ;; └─┘
   (split-window-below)
 
-  ;; +-+-+
-  ;; | | |
-  ;; +-+-+
-  ;; |   |
-  ;; +---+
+  ;; ┌─┬─┐
+  ;; │ │ │
+  ;; ├─┴─┤
+  ;; │   │
+  ;; └───┘
   (ignore-errors (windmove-up))
   (split-window-right)
 
-  ;; +-+-+
-  ;; | | |
-  ;; +-+ |
-  ;; | | |
-  ;; +-+-+
-  ;; |   |
-  ;; +---+
+  ;; ┌─┬─┐
+  ;; │ │ │
+  ;; ├─┤ │
+  ;; │ │ │
+  ;; ├─┴─┤
+  ;; │   │
+  ;; └───┘
   (ignore-errors (windmove-up))
   (ignore-errors (windmove-left))
   (split-window-below)
 
-  ;; +-+-+
-  ;; | | |
-  ;; +-+-+
-  ;; | | |
-  ;; +-+-+
-  ;; |   |
-  ;; +---+
+  ;; ┌─┬─┐
+  ;; │ │ │
+  ;; ├─┼─┤
+  ;; │ │ │
+  ;; ├─┴─┤
+  ;; │   │
+  ;; └───┘
   (ignore-errors (windmove-up))
   (ignore-errors (windmove-up))
   (ignore-errors (windmove-right))
@@ -55,55 +54,56 @@
   (ignore-errors (windmove-right))
   (ignore-errors (windmove-right))
   (split-window-below)
-  )
 
-;; TODO: Balance the sizes
+  (balance-windows))
+
 (defun windows-wide ()
   (interactive)
-  ;; +-+
-  ;; | |
-  ;; +-+
+  ;; ┌─┐
+  ;; │ │
+  ;; └─┘
   (delete-other-windows)
 
-  ;; +-+-+
-  ;; | | |
-  ;; +-+-+
+  ;; ┌─┬─┐
+  ;; │ │ │
+  ;; └─┴─┘
   (split-window-right)
 
-  ;; +-+-+-+
-  ;; | | | |
-  ;; +-+-+-+
+  ;; ┌─┬─┬─┐
+  ;; │ │ │ │
+  ;; └─┴─┴─┘
   (ignore-errors (windmove-right))
   (split-window-right)
 
-  ;; +-+-+-+
-  ;; | | | |
-  ;; +-+ | |
-  ;; | | | |
-  ;; +-+-+-+
+  ;; ┌─┬─┬─┐
+  ;; │ │ │ │
+  ;; ├─┤ │ │
+  ;; │ │ │ │
+  ;; └─┴─┴─┘
   (ignore-errors (windmove-left))
   (ignore-errors (windmove-left))
   (split-window-below)
 
-  ;; +-+-+-+
-  ;; | | | |
-  ;; +-+-+ |
-  ;; | | | |
-  ;; +-+-+-+
+  ;; ┌─┬─┬─┐
+  ;; │ │ │ │
+  ;; ├─┼─┤ │
+  ;; │ │ │ │
+  ;; └─┴─┴─┘
   (ignore-errors (windmove-left))
   (ignore-errors (windmove-left))
   (ignore-errors (windmove-right))
   (split-window-below)
 
-  ;; +-+-+-+
-  ;; | | | |
-  ;; +-+-+-+
-  ;; | | | |
-  ;; +-+-+-+
+  ;; ┌─┬─┬─┐
+  ;; │ │ │ │
+  ;; ├─┼─┼─┤
+  ;; │ │ │ │
+  ;; └─┴─┴─┘
   (ignore-errors (windmove-right))
   (ignore-errors (windmove-right))
   (split-window-below)
-  )
+
+  (balance-windows))
 
 ;; TODO: Keybinding which picks one of these, based on frame size
 
