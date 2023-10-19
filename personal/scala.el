a713b6c273713718a51e6bd8a379b672bda23443
Chris Warburton <chriswarbo@gmail.com>
2023-10-19 02:49:17 +0100

    Remove sbt-mode, which causes errors

--- personal/scala.el
+++ personal/scala.el
@@ -3,26 +3,6 @@
   :mode "\\.s\\(cala\\|bt\\)$"
   :bind (("C-c C-c" . sbt-run-previous-command)))
 
-(use-package sbt-mode
-  :ensure t
-  :pin melpa-stable
-  :commands sbt-start sbt-command
-  :config
-  ;; WORKAROUND: https://github.com/ensime/emacs-sbt-mode/issues/31
-  ;; allows using SPACE when in the minibuffer
-  (substitute-key-definition
-   'minibuffer-complete-word
-   'self-insert-command
-   minibuffer-local-completion-map)
-  (setq sbt:default-command "test")
-  (setq sbt:program-options
-        '("-Dsbt.color=always"
-          "-Dsbt.log.noformat=false"
-          ;; See https://github.com/hvesalai/emacs-sbt-mode/issues/139
-          "-Djline.terminal=auto"
-          ;; See https://github.com/hvesalai/emacs-sbt-mode/issues/152
-          "-Dsbt.supershell=false"
-          )))
 ;; Enable nice rendering of diagnostics like compile errors.
 (use-package flycheck
   :ensure t
