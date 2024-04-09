ade60bfd46740c7aa175a7b9bfe9ae6f77f3909d
Chris Warburton <chriswarbo@gmail.com>
2024-04-09 01:54:58 +0100

    Use GCR for ssh-agent

--- personal/tramp.el
+++ personal/tramp.el
@@ -9,4 +9,4 @@
 (setq projectile-mode-line "Projectile")
 
 ;; TODO: Take from ~/.bashrc, e.g. by running a bash script and parsing it out
-(setenv "SSH_AUTH_SOCK" "/run/user/1000/keyring/ssh")
+(setenv "SSH_AUTH_SOCK" "/run/user/1000/gcr/ssh")
