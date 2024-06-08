d595408bd293bae571504b100e2f0cd77c3bfc16
Chris Warburton <chriswarbo@gmail.com>
2024-06-08 12:32:04 +0100

    fiddle with package archives

--- early-init.el
+++ early-init.el
@@ -10,10 +10,10 @@
 
 ;(add-to-list 'package-archives
 ;             '("gnu"       . "http://elpa.gnu.org/packages/"))
-;(add-to-list 'package-archives
-;             '("melpa"     . "https://melpa.org/packages/"))
 (add-to-list 'package-archives
-             '("melpa-stable" . "https://stable.melpa.org/packages/") t)
+            '("melpa"     . "https://melpa.org/packages/"))
+;; (add-to-list 'package-archives
+;;              '("melpa-stable" . "https://stable.melpa.org/packages/") t)
 
 (provide 'early-init)
 ;;; early-init.el ends here
