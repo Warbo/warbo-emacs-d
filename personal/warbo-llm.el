d39079d221acb18842fa32e67a8351a23adde7bf
Chris Warburton <chris.warburton@tracsis.com>
2025-04-23 16:29:30 +0100

    warbo-llm: bias model away from newbie tutorial suggestions

--- personal/warbo-llm.el
+++ personal/warbo-llm.el
@@ -18,5 +18,15 @@
              :chat-model model
              :embedding-model model)))
 
+  ;; Hint that we want in-depth analysis, not hand-holding
+  (setopt ellama-user-nick "SeniorDev")
+
+  ;; Useful to see what it's doing
+  (setopt ellama-always-show-chain-steps t)
+
+  ;; Alter prompts to try and reduce annoying behaviours
+  (setopt ellama-code-review-prompt-template
+          "You are a senior programmer. You need to review the given code. Identify the most important issues, if any; don't get hung up on minor quibbles with style/opinion. The code was written by a senior dev, who should know what they're doing; keep this in mind when looking for issues (might there be a good reason for what's been done?). Be concise, and to the point; further discussion can be had later."))
+
 (provide 'warbo-llm)
 ;;; warbo-llm.el ends here
