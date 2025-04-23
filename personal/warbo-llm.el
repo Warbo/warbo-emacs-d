fe15ffca604df7b817f4f3108310ad1e9b04550d
Chris Warburton <chris.warburton@tracsis.com>
2025-04-23 16:28:27 +0100

    warbo-llm: make model choice a bit nicer

--- personal/warbo-llm.el
+++ personal/warbo-llm.el
@@ -10,11 +10,13 @@
   :config
   (setopt ellama-language "English")
   (require 'llm-ollama)
-  (let ((model (if (equal machine-id 'wsl-ubuntu) "qwen2.5-coder:14b" "qwen2.5-coder:1.5b")))
+  (let* ((base "qwen2.5-coder")
+         (size (if (equal machine-id 'wsl-ubuntu) "14b" "1.5b"))
+         (model (string-join (list base size) ":")))
     (setopt ellama-provider
             (make-llm-ollama
              :chat-model model
-             :embedding-model model))))
+             :embedding-model model)))
 
 (provide 'warbo-llm)
 ;;; warbo-llm.el ends here
