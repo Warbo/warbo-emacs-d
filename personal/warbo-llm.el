bba34d02220ed2660374ecc2a15fba753ec79cc6
Your Name <you@example.com>
2025-05-16 15:33:20 +0100

    Add aider

--- personal/warbo-llm.el
+++ personal/warbo-llm.el
@@ -5,18 +5,18 @@
 (use-package llm
   :ensure t)
 
+(defvar warbo-local-llm "qwen3"
+  "Name of LLM model to use with ollama.")
+
 (use-package ellama
   :ensure t
   :config
   (setopt ellama-language "English")
   (require 'llm-ollama)
-  (let* ((base "qwen2.5-coder")
-         (size (if (equal machine-id 'wsl-ubuntu) "14b" "1.5b"))
-         (model (string-join (list base size) ":")))
-    (setopt ellama-provider
-            (make-llm-ollama
-             :chat-model model
-             :embedding-model model)))
+  (setopt ellama-provider
+          (make-llm-ollama
+           :chat-model warbo-local-llm
+           :embedding-model warbo-local-llm))
 
   ;; Hint that we want in-depth analysis, not hand-holding
   (setopt ellama-user-nick "SeniorDev")
@@ -27,6 +27,12 @@
   ;; Alter prompts to try and reduce annoying behaviours
   (setopt ellama-code-review-prompt-template
           "You are a senior programmer. You need to review the given code. Identify the most important issues, if any; don't get hung up on minor quibbles with style/opinion. The code was written by a senior dev, who should know what they're doing; keep this in mind when looking for issues (might there be a good reason for what's been done?). Be concise, and to the point; further discussion can be had later."))
+
+(use-package aider
+  :ensure
+  :config
+  (setq aider-args `("--model" ,(concat "ollama_chat/" warbo-local-llm)))
+  (global-set-key (kbd "C-c a") 'aider-transient-menu))
 
 (provide 'warbo-llm)
 ;;; warbo-llm.el ends here
