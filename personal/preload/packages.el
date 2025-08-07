fb4a9f457e13637a98e6af470542768576b97e38
Your Name <you@example.com>
2025-08-07 11:09:40 +0100

    Remove commented-out use-package lines which we have elsewhere
    There's a list of old packages in personal/preload/packages.el, which I've been
    meaning to look through to see if I want to re-enable (via use-package, rather
    than the imperative approaches I used to get them before).
    
    However, lots of them are *already* enabled via use-package, so there's no point
    having them in this TODO list.

--- personal/preload/packages.el
+++ personal/preload/packages.el
@@ -4,61 +4,27 @@
 
 ;; (use-package ace-jump-mode)
 
-;; (use-package ace-window)
-
 ;; (use-package ahg)
-
-;; (use-package anzu)
 
 ;; (use-package arduino-mode)
 
 ;; (use-package async)
 
-;; (use-package auctex)
-
-;; (use-package avy)
-
-;; (use-package beacon)
-
 ;; (use-package bind-key)
-
-;; (use-package browse-kill-ring)
 
 ;; (use-package caml)
 
-;; (use-package cmake-mode)
-
 ;; (use-package counsel)
-
-;; (use-package crux)
-
-;; (use-package csv-mode)
 
 ;; (use-package dante)
 
-;; (use-package dash)
-
 ;; (use-package dash-functional)
-
-;; (use-package diff-hl)
-
-;; (use-package diminish)
-
-;; (use-package discover-my-major)
 
 ;; (use-package dumb-jump)
 
-;; (use-package easy-kill)
-
 ;; (use-package elisp-slime-nav)
 
-;; (use-package epl)
-
 ;; (use-package ess)
-
-;; (use-package expand-region)
-
-;; (use-package f)
 
 ;; (use-package faceup)
 
@@ -80,21 +46,11 @@
 
 ;; (use-package gh)
 
-;; (use-package git-commit)
-
 ;; (use-package gitconfig-mode)
 
 ;; (use-package gitignore-mode)
 
 ;; (use-package gnuplot)
-
-;; (use-package god-mode)
-
-;; (use-package grizzl)
-
-;; (use-package guru-mode)
-
-;; (use-package haskell-mode)
 
 ;; (use-package highlight)
 
@@ -109,8 +65,6 @@
 ;; (use-package idris-mode)
 
 ;; (use-package iedit)
-
-;; (use-package imenu-anywhere)
 
 ;; (use-package jabber)
 
@@ -134,8 +88,6 @@
 
 ;; (use-package lsp-haskell)
 
-;; (use-package lsp-mode)
-
 ;; (use-package macrostep)
 
 ;; (use-package makey)
@@ -150,8 +102,6 @@
 
 ;; (use-package monky)
 
-;; (use-package move-text)
-
 ;; (use-package multiple-cursors)
 
 ;; (use-package nix-buffer)
@@ -164,25 +114,15 @@
 
 ;; (use-package o-blog)
 
-;; (use-package operate-on-number)
-
-;; (use-package ov)
-
 ;; (use-package package-build)
 
 ;; (use-package parsebib)
 
 ;; (use-package pcache)
 
-;; (use-package php-mode)
-
 ;; (use-package pkg-info)
 
-;; (use-package projectile)
-
 ;; (use-package prop-menu)
-
-;; (use-package racket-mode)
 
 ;; (use-package realgud)
 
@@ -190,15 +130,11 @@
 
 ;; (use-package ruby-tools)
 
-;; (use-package s)
-
 ;; (use-package seq)
 
 ;; (use-package shadchen)
 
 ;; (use-package slime)
-
-;; (use-package smartrep)
 
 ;; (use-package smex)
 
@@ -208,23 +144,11 @@
 
 ;; (use-package test-simple)
 
-;; (use-package tuareg)
-
-;; (use-package use-package)
-
-;; (use-package volatile-highlights)
-
 ;; (use-package wc-mode)
-
-;; (use-package which-key)
 
 ;; (use-package with-editor)
 
 ;; (use-package ws-butler)
-
-;; (use-package yasnippet)
-
-;; (use-package zop-to-char)
 
 ;; (use-package zoutline)
 
@@ -234,15 +158,11 @@
 ;; "ido-completing-read+"
 ;; "ido-ubiquitous"
 ;; "image+"
-;; "isar-mode"
-;; "metal-mercury-mode"
 ;; "prelude-emacs-lisp"
 ;; "prelude-haskell"
 ;; "prelude-js"
 ;; "prelude-lisp"
 ;; "scala-mode2"
-;; "simp-isar-mode"
-;; "so-long"
 ;; "sunrise-commander"
 ;; "sunrise-x-buttons"
 ;; "sunrise-x-loop"
