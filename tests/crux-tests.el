88e1c943dc786fdceeaa5aa083ade04318473423
Chris Warburton <chriswarbo@gmail.com>
2025-08-19 21:04:32 +0100

    Fix crux tests

--- tests/crux-tests.el
+++ tests/crux-tests.el
@@ -5,12 +5,12 @@
 ;;; Test `crux-with-region-or-buffer'
 
 (defun crux-buffer-test-function (&optional start end)
-  "A test function that returns the start and end positions."
+  "Return the given START and END positions."
   (interactive)
   (list start end))
 
 (ert-deftest crux-with-region-or-buffer-non-interactive-no-args-no-region ()
-  "Test `crux-with-region-or-buffer` non-interactively with no arguments or region."
+  "Test `crux-with-region-or-buffer` non-interactively with no args or region."
   (crux-with-region-or-buffer crux-buffer-test-function)
   (unwind-protect
       (with-temp-buffer
@@ -19,7 +19,7 @@
     (advice-remove 'crux-buffer-test-function 'with-region-or-buffer)))
 
 (ert-deftest crux-with-region-or-buffer-non-interactive-no-args-with-region ()
-  "Test `crux-with-region-or-buffer` non-interactively with no arguments but a region."
+  "Test `crux-with-region-or-buffer` non-interactively with no args but a region."
   (crux-with-region-or-buffer crux-buffer-test-function)
   (unwind-protect
       (with-temp-buffer
@@ -30,7 +30,7 @@
     (advice-remove 'crux-buffer-test-function 'with-region-or-buffer)))
 
 (ert-deftest crux-with-region-or-buffer-non-interactive-with-args ()
-  "Test `crux-with-region-or-buffer` non-interactively with arguments."
+  "Test `crux-with-region-or-buffer` non-interactively with args."
   (crux-with-region-or-buffer crux-buffer-test-function)
   (unwind-protect
       (with-temp-buffer
@@ -63,22 +63,22 @@
 ;;; Test `crux-with-region-or-line'
 
 (defun crux-line-test-function (&optional start end)
-  "A test function that returns the start and end positions."
+  "Return the given START and END positions."
   (interactive)
   (list start end))
 
 (ert-deftest crux-with-region-or-line-non-interactive-no-args ()
-  "Test `crux-with-region-or-line` non-interactively with no arguments."
+  "Test `crux-with-region-or-line` non-interactively with no args."
   (crux-with-region-or-line crux-line-test-function)
   (unwind-protect
       (with-temp-buffer
         (insert "line 1\nline 2\nline 3")
         (goto-char 10)
-        (should (equal (list 8 14) (crux-line-test-function))))
+        (should (equal (list 8 15) (crux-line-test-function))))
     (advice-remove 'crux-line-test-function 'with-region-or-line)))
 
 (ert-deftest crux-with-region-or-line-non-interactive-with-args ()
-  "Test `crux-with-region-or-line` non-interactively with arguments."
+  "Test `crux-with-region-or-line` non-interactively with args."
   (crux-with-region-or-line crux-line-test-function)
   (unwind-protect
       (with-temp-buffer
@@ -106,13 +106,13 @@
         (insert "line 1\nline 2\nline 3")
         (goto-char 10)
         (let ((last-command this-command))
-          (should (equal (list 8 14) (call-interactively 'crux-line-test-function)))))
+          (should (equal (list 8 15) (call-interactively 'crux-line-test-function)))))
     (advice-remove 'crux-line-test-function 'with-region-or-line)))
 
 ;;; Test `crux-with-region-or-sexp-or-line'
 
 (defun crux-sexp-test-function (&optional start end)
-  "A test function that returns the start and end positions."
+  "Return the given START and END positions."
   (interactive)
   (list start end))
 
@@ -123,7 +123,7 @@
       (with-temp-buffer
         (insert "(foo bar)\nbaz")
         (goto-char 2)
-        (should (equal (list 1 9) (crux-sexp-test-function))))
+        (should (equal (list 1 10) (crux-sexp-test-function))))
     (advice-remove 'crux-sexp-test-function 'with-region-or-sexp-or-line)))
 
 (ert-deftest crux-with-region-or-sexp-or-line-non-interactive-string ()
@@ -133,7 +133,7 @@
       (with-temp-buffer
         (insert "\"foo bar\"\nbaz")
         (goto-char 2)
-        (should (equal (list 1 9) (crux-sexp-test-function))))
+        (should (equal (list 1 10) (crux-sexp-test-function))))
     (advice-remove 'crux-sexp-test-function 'with-region-or-sexp-or-line)))
 
 (ert-deftest crux-with-region-or-sexp-or-line-non-interactive-line ()
@@ -142,8 +142,8 @@
   (unwind-protect
       (with-temp-buffer
         (insert "(foo bar)\nbaz")
-        (goto-char 10)
-        (should (equal (list 10 13) (crux-sexp-test-function))))
+        (goto-char 11)
+        (should (equal (list 11 14) (crux-sexp-test-function))))
     (advice-remove 'crux-sexp-test-function 'with-region-or-sexp-or-line)))
 
 (ert-deftest crux-with-region-or-sexp-or-line-interactive-sexp ()
@@ -154,18 +154,18 @@
         (insert "(foo bar)\nbaz")
         (goto-char 2)
         (let ((last-command this-command))
-          (should (equal (list 1 9) (call-interactively 'crux-sexp-test-function)))))
+          (should (equal (list 1 10) (call-interactively 'crux-sexp-test-function)))))
     (advice-remove 'crux-sexp-test-function 'with-region-or-sexp-or-line)))
 
 ;;; Test `crux-with-region-or-point-to-eol'
 
 (defun crux-eol-test-function (&optional start end)
-  "A test function that returns the start and end positions."
+  "Return the given START and END positions."
   (interactive)
   (list start end))
 
 (ert-deftest crux-with-region-or-point-to-eol-non-interactive-no-args ()
-  "Test `crux-with-region-or-point-to-eol` non-interactively with no arguments."
+  "Test `crux-with-region-or-point-to-eol` non-interactively with no args."
   (crux-with-region-or-point-to-eol crux-eol-test-function)
   (unwind-protect
       (with-temp-buffer
@@ -175,7 +175,7 @@
     (advice-remove 'crux-eol-test-function 'with-region-or-point-to-eol)))
 
 (ert-deftest crux-with-region-or-point-to-eol-non-interactive-with-args ()
-  "Test `crux-with-region-or-point-to-eol` non-interactively with arguments."
+  "Test `crux-with-region-or-point-to-eol` non-interactively with args."
   (crux-with-region-or-point-to-eol crux-eol-test-function)
   (unwind-protect
       (with-temp-buffer
