9404022138e52e5496d992af800947752bcd63aa
Your Name <you@example.com>
2025-10-24 11:42:10 +0100

    warbo-shells: we don't use ansi-term
    Turns out we were globally binding <f2> to ansi-term, but I don't really use it.
    However, now that I'm making occasional use of mistty, this would actually be
    quite handy for that!

--- personal/warbo-shells.el
+++ personal/warbo-shells.el
@@ -57,6 +57,22 @@
     "/bin/sh")
   "A list of paths where we might find a shell binary, in order of preference.")
 
+;; TODO: Globally bind <f2> to create/switch-to a mistty buffer, depending on
+;; CWD:
+;;  - If CWD is in a git repo, look for mistty buffers named for that repo (and
+;;    this TRAMP host, if we're using one)
+;;    - If there are multiple (disambiguated with numbers), choose interactively
+;;    - If we're not in a git repo, use CWD in its place
+;;    - If there are no existing buffers found, create one
+;;  - When creating, the buffer name should include:
+;;    - The current TRAMP host, if there is one
+;;    - The name of the git repo we're inside, or CWD if we're not in one
+;;    - Optionally a number to disambiguate, if we already have mistty buffers
+;;      for this host & repo/CWD.
+;;  - If we're already in a mistty buffer, open another one with the same TRAMP
+;;    host & repo/CWD, and use a fresh number to disambiguate.
+;;  - The CWD of a freshly created mistty buffer should be the same as where we
+;;    opened it from; e.g. don't go to the git toplevel, that's just for naming.
 (use-package mistty
   :ensure t
   :bind ((:map mistty-prompt-map
@@ -457,37 +473,6 @@
   (when (get-buffer "*Async Shell Command*")
     (with-current-buffer "*Async Shell Command*"
       (rename-uniquely))))
-
-;; From http://www.enigmacurry.com/2008/12/26/emacs-ansi-term-tricks/
-(defer (lambda ()
-         (require 'term)
-         (defun visit-ansi-term ()
-           "If the current buffer is:
-     1) a running ansi-term named *ansi-term*, rename it.
-     2) a stopped ansi-term, kill it and create a new one.
-     3) a non ansi-term, go to an already running ansi-term
-        or start a new one while killing a defunt one"
-           (interactive)
-           (let ((is-term (string= "term-mode" major-mode))
-                 (is-running (term-check-proc (buffer-name)))
-                 (term-cmd "bash")
-                 (anon-term (get-buffer "*ansi-term*")))
-             (if is-term
-                 (if is-running
-                     (if (string= "*ansi-term*" (buffer-name))
-                         (call-interactively 'rename-buffer)
-                       (if anon-term
-                           (switch-to-buffer "*ansi-term*")
-                         (ansi-term term-cmd)))
-                   (kill-buffer (buffer-name))
-                   (ansi-term term-cmd))
-               (if anon-term
-                   (if (term-check-proc "*ansi-term*")
-                       (switch-to-buffer "*ansi-term*")
-                     (kill-buffer "*ansi-term*")
-                     (ansi-term term-cmd))
-                 (ansi-term term-cmd)))))
-         (global-set-key (kbd "<f2>") 'visit-ansi-term)))
 
 (custom-set-variables
  '(comint-scroll-to-bottom-on-input nil)  ; allow inserting anywhere
