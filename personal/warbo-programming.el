f440ce3d6a28febd1a4ab1077035b686a0cc6c77
Chris Warburton <chriswarbo@gmail.com>
2026-01-27 16:12:06 +0000

    Consolidate yasnippet code

--- personal/warbo-programming.el
+++ personal/warbo-programming.el
@@ -601,7 +601,48 @@ with the string S. Unlike `replace-region-contents' this maintains text
   (add-to-list 'corfu-margin-formatters #'kind-icon-margin-formatter))
 
 (use-package yasnippet
-  :ensure t)
+  :ensure t
+  :config
+  (yas-reload-all)
+  (add-hook 'prog-mode-hook 'yas-minor-mode)
+  (add-hook 'text-mode-hook 'yas-minor-mode)
+
+  (defun yasnippet-or-completion ()
+    "Try to expand a yasnippet snippet, otherwise invoke completion."
+    (interactive)
+    (or (do-yas-expand)
+        (completion-at-point)))
+
+  (defun check-expansion ()
+    "Return t if point is at a location where completion is likely.
+This is the case if point is at the end of a symbol, or after a `.', or
+after `::'."
+    (save-excursion
+      (if (looking-at "\\_>") t
+        (backward-char 1)
+        (if (looking-at "\\.") t
+          (backward-char 1)
+          (if (looking-at "::") t nil)))))
+
+  (defun do-yas-expand ()
+    "Try to expand a yasnippet snippet, returning nil on failure."
+    (let ((yas/fallback-behavior 'return-nil))
+      (yas/expand)))
+
+  (defun tab-indent-or-complete ()
+    "Indent the current line, or complete the current symbol.
+If the minibuffer is active, then completion is performed.  Otherwise,
+if yasnippet is active and a snippet can be expanded, that is done.
+Otherwise, if at a point where completion is likely, completion is
+invoked.  Otherwise, the current line is indented."
+    (interactive)
+    (if (minibufferp)
+        (minibuffer-complete)
+      (if (or (not yas/minor-mode)
+              (null (do-yas-expand)))
+          (if (check-expansion)
+              (completion-at-point)
+            (indent-for-tab-command))))))
 
 ;; Posframe is a pop-up tool that must be manually installed for dap-mode
 (use-package posframe
--- personal/warbo-scala.el
+++ personal/warbo-scala.el
@@ -8,12 +8,5 @@
   :bind (:map scala-mode-map
               ("C-c C-c" . sbt-run-previous-command)))
 
-(use-package yasnippet
-  :ensure t)
-
-;; Posframe is a pop-up tool that must be manually installed for dap-mode
-(use-package posframe
-  :ensure t)
-
 (provide 'warbo-scala)
 ;;; warbo-scala.el ends here
