b119ee18a5678d073b698e000649435a66493818
Chris Warburton <chriswarbo@gmail.com>
2026-02-05 00:27:30 +0000

    pi: avoid annoying colours from whitespace-mode
    Most egregious is that pi will purposefully put an empty line at the end of the
    chat (fine), but whitespace-mode will colour that in yellow. Since pi delays the
    fontification of chat messages (to avoid being too slow), we end up with new
    messages having a yellow background, which makes the white text unreadable.

--- personal/warbo-llm.el
+++ personal/warbo-llm.el
@@ -17,8 +17,13 @@
   :config
   (global-set-key (kbd "C-c a") 'aidermacs-transient-menu))
 
+(defun warbo-ignore-pi-whitespace ()
+  "Disable `whitespace-mode' in pi chat buffers."
+  (whitespace-mode -1))
+
 (use-package pi-coding-agent
   :ensure t
+  :hook (pi-coding-agent-chat-mode . warbo-ignore-pi-whitespace)
   :init (defalias 'pi 'pi-coding-agent))
 
 (provide 'warbo-llm)
