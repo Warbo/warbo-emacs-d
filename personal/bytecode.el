afa1fc88881c731764529c408370a3cc4f24a69d
Chris Warburton <chriswarbo@gmail.com>
2026-02-05 01:19:48 +0000

    Native compilation

--- personal/bytecode.el
+++ personal/bytecode.el
@@ -25,7 +25,17 @@
              (make-directory (file-name-directory dest) t)
              dest))))
     (message "Byte-compiling files from %s into %s..." source-dir target-cache-dir)
-    (byte-recompile-directory source-dir 0 t)))
+    (byte-recompile-directory source-dir 0 t))
+
+  ;; Native compilation (if available)
+  (when (and (fboundp 'native-comp-available-p)
+             (native-comp-available-p))
+    (let* ((eln-cache-dir (expand-file-name "eln-cache" target-cache-dir))
+           (native-comp-eln-load-path (cons eln-cache-dir native-comp-eln-load-path)))
+      (make-directory eln-cache-dir t)
+      (message "Native-compiling files from %s into %s..." source-dir eln-cache-dir)
+      ;; Compile all .el files in the source directory
+      (native-compile-async source-dir t t))))
 
 (provide 'bytecode)
 
