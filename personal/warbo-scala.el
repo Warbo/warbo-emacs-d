acc0e2716aa7114ee58b6d947083783ceaec180f
Chris Warburton <chriswarbo@gmail.com>
2026-02-13 17:41:22 +0000

    Minor fixes

--- personal/warbo-scala.el
+++ personal/warbo-scala.el
@@ -5,10 +5,10 @@
 (use-package scala-ts-mode
   :ensure t
   :mode "\\.s\\(cala\\|bt\\)$"
-  :bind (:map scala-mode-map
+  :bind (:map scala-ts-mode-map
               ("C-c C-c" . sbt-run-previous-command))
   :config
-  (add-hook 'scala-mode-hook 'scala-format-on-save-mode))
+  (add-hook 'scala-ts-mode-hook 'scala-format-on-save-mode))
 
 (provide 'warbo-scala)
 ;;; warbo-scala.el ends here
--- personal/warbo-web.el
+++ personal/warbo-web.el
@@ -13,14 +13,20 @@
   :config
   (add-to-list 'major-mode-remap-alist '(html-mode . html-ts-mode))
   (add-to-list 'major-mode-remap-alist '(web-mode . html-ts-mode))
-  ;; FIXME: Change these to html-ts-mode equivalents
-  (setq web-mode-code-indent-offset 2
-        web-mode-css-indent-offset 2
-        web-mode-markup-indent-offset 2
-        web-mode-sql-indent-offset 2
-        web-mode-script-padding 0 ;; start script in col 0
-        web-mode-enable-current-column-highlight t
-        ))
+  ;; FIXME: Change these to html-ts-mode equivalents:
+  ;; HTML Indent   web-mode-markup-indent-offset             html-ts-mode-indent-offset (or tab-width)
+  ;; CSS Indent    web-mode-css-indent-offset                css-ts-mode-indent-offset
+  ;; JS/TS Indent  web-mode-code-indent-offset               js-ts-mode-indent-offset
+  ;; Padding       web-mode-script-padding                   Not supported (indentation is structural)
+  ;; Highlight     web-mode-enable-current-column-highlight  Use external package (e.g., column-highlight-mode)
+  ;; (setq web-mode-code-indent-offset 2
+  ;;       web-mode-css-indent-offset 2
+  ;;       web-mode-markup-indent-offset 2
+  ;;       web-mode-sql-indent-offset 2
+  ;;       web-mode-script-padding 0 ;; start script in col 0
+  ;;       web-mode-enable-current-column-highlight t
+  ;;       )
+  )
 
 (use-package css-mode
   :mode (("\\.css\\'" . css-ts-mode))
@@ -46,17 +52,17 @@
     '("vue-language-server" "--stdio"
       :initializationOptions
       (:typescript (:tsdk "node_modules/typescript/lib")
-                   :vue (:hybridMode :json-false)
-                   :languageFeatures (:completion (:defaultTagNameCase "both"
-                                                                       :defaultAttrNameCase "kebabCase"
-                                                                       :getDocumentNameCasesRequest nil
-                                                                       :getDocumentSelectionRequest nil)
-                                                  :diagnostics (:getDocumentVersionRequest nil))
-                   :documentFeatures
-                   (:documentFormatting (:defaultPrintWidth 100
-                                                            :getDocumentPrintWidthRequest nil)
-                                        :documentSymbol t
-                                        :documentColor t)))
+       :vue (:hybridMode :json-false)
+             :languageFeatures (:completion (:defaultTagNameCase "both"
+                                             :defaultAttrNameCase "kebabCase"
+                                             :getDocumentNameCasesRequest nil
+                                             :getDocumentSelectionRequest nil)
+                                :diagnostics (:getDocumentVersionRequest nil))
+             :documentFeatures (:documentFormatting
+                                (:defaultPrintWidth 100
+                                 :getDocumentPrintWidthRequest nil)
+                                :documentSymbol t
+                                :documentColor t)))
     "Eglot server program entry for vue-ts-mode.
 Use in .dir-locals.el like:
   (eglot-server-programs . ((vue-ts-mode . ,warbo-vue-eglot-args)))")
